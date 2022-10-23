180302a26dcc181006a5730c4000bfee0347a4ee
Nghia Nguyen <nghianguyenservice@gmail.com>
2022-10-22 19:11:51 -0500

    setting up structure of login page

---
+++ cms_app/src/Components/Screens/cms_login_screen.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import LoginModule from '../login_form_module.jsx';
+
+export default class cms_login_screen extends Component {
+  render() {
+    return (
+      <div>
+        <LoginModule/>
+      </div>
+    )
+  }
+}
---
+++ cms_app/src/Components/login_form_module.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class login_form_module extends Component {
+  render() {
+    return (
+      <div>login_form_module</div>
+    )
+  }
+}
