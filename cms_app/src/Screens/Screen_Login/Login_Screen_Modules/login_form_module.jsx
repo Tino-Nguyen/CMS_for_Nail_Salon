f0994f827d4a4ce5ec2cb201dd11ddd8fd7b07cc
Nghia Nguyen <nghianguyenservice@gmail.com>
2022-10-24 11:32:13 -0500

    added a submit button

--- cms_app/src/Screens/Screen_Login/Login_Screen_Modules/login_form_module.jsx
+++ cms_app/src/Screens/Screen_Login/Login_Screen_Modules/login_form_module.jsx
@@ -3,7 +3,17 @@ import React, { Component } from 'react'
 export default class login_form_module extends Component {
   render() {
     return (
-      <div>login_form_module</div>
+      <div>
+        <form>
+          <label for="name_input">Username:</label>
+          <input type="text" id="name_input"/>
+          <br/>
+          <label for="password_input">Password:</label>
+          <input type="password" id="password_input"/>
+          <br/>
+          <input type="submit"/>
+        </form>
+      </div>
     )
   }
 }
